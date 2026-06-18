24ac4dfa09ca86f37cc17b11daaa41206813dd90
sf\adhvi <adhvi@fylkesmannen.no>
2026-06-18 14:03:51 +0200

    Added script for updating player name in db

---
+++ Data/hotfixes/UpdateNameForPlayer.sql
@@ -0,0 +1,18 @@
+﻿BEGIN TRAN;
+
+UPDATE dbo.AspNetUsers
+SET FullName = N'Kent Monsen'
+WHERE Id = 'dd58d1e6-bd51-46b8-8abd-42b0522314a2'
+  AND FullName = N'kentmonsen@gmail.con';
+
+IF @@ROWCOUNT <> 1
+BEGIN
+ROLLBACK;
+THROW 50001, 'Forventa å oppdatere nøyaktig éin brukar med FullName = kentmonsen@gmail.con.', 1;
+END;
+
+SELECT Id, UserName, Email, FullName
+FROM dbo.AspNetUsers
+WHERE Id = 'dd58d1e6-bd51-46b8-8abd-42b0522314a2';
+
+COMMIT;
